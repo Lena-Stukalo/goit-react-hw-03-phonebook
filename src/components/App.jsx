28a415f94431c013110b39affced45933ed3b733
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-08-07 20:07:32 +0300

    local

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,23 @@ import Form from './form/Form';
 import Contacts from './contacts/Contacts';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+  componentDidMount() {
+    console.log('componentDidMount');
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate');
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   checkName = name => {
     const { contacts } = this.state;
     return contacts.find(contact => contact.name === name);
